d612b21401a070ca71aa770495e40161ceb59ed2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-08 20:45:55 +0300

    form usestate

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Hello from "./components/Hello";
 import Myname from "./components/Myname";
 import Title from "./components/Title";
 import Welcome from "./components/Welcome";
-
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
       <Title name="Solidity Devloper" />
       <Title name="FullStack Devloper" />
       <Myname />
+      <Form />
     </div>
   );
 }
